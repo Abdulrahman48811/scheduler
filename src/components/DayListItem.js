dd558fcb965edf21a3a26ec9cf835e81e1f8bc68
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-01-19 07:09:24 -0800

    daylistitem test & formatspots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,8 +7,8 @@ export default function DayListItem(props) {
   const {name, spots, selected, setDay} = props;
   const dayClass = classNames(
     "day-list__item",
-    {"day-list__item--selected": selected,
-    "day-list__item--full": spots === 0
+    {"day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
     });
 
   const formatSpots = (spots) => {
